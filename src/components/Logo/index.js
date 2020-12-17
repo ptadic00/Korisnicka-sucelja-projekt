ce2c1b7df02d9b1bc1e5dece58875a702ea04dbf
Kristina <kramlj00@fesb.hr>
2020-12-17 08:47:50 +0100

    Adding Logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const Logo = () => (
+  <section className={styles.logo}>
+    <span className={styles.logoText}>Music Band 100's</span>
+    <span className={styles.sub}>BEND ZA SVADBE</span>
+  </section>
+)
+
+export default Logo
