8b9e3c446a8af0fff711d32f49eef96206980728
Kristina <kramlj00@fesb.hr>
2020-12-18 17:47:29 +0100

    "Adding BandMembers"

---
+++ src/components/BandMembers/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import styles from './style.module.css';
+
+import i1 from '../../images/member1.jpg'
+import i2 from '../../images/member2.jpg'
+import i3 from '../../images/member3.jpg'
+
+const BandMembers = () => (
+    <nav>
+        <h2 className={styles.title}>Članovi benda</h2>
+        <div className={styles.members}>
+            <article className={styles.member1}>
+                <img src={i1} alt="member1" className={styles.member_img} width="300px" height="400px"></img>
+                <h4>
+                    Marijana Cikatić
+                    <br></br>
+                    (vokalistica)
+                </h4>
+                <p className={styles.descr}>
+                    Nešto o tom članu benda. Kratka biografija.
+                </p>
+        </article>
+        <article className={styles.member1}>
+                <img src={i2} alt="member2" className={styles.member_img} width="300px" height="400px"></img>
+                <h4>
+                    Filip Matić
+                    <br></br>
+                    (vokal i klavijature)
+                </h4>
+                <p className={styles.descr}>
+                    Nešto o tom članu benda. Kratka biografija.
+                </p>
+        </article>
+        <article className={styles.member1}>
+                <img src={i3} alt="member3" className={styles.member_img} width="300px" height="400px"></img>
+                <h4>
+                    Zoran Vučičić
+                    <br></br>
+                    (vokal i gitara)
+                </h4>
+                <p className={styles.descr}>
+                    Nešto o tom članu benda. Kratka biografija.
+                </p>
+        </article>
+        </div>
+    </nav>
+)
+
+export default BandMembers
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import NavigationHeader from '../components/NavigationHeader'
 import Footer from '../components/Footer'
 import Carousel from '../components/Carousel'
 import AboutUs from '../components/AboutUs'
+import BandMembers from '../components/BandMembers'
 
 const IndexPage = () => (
   <main>
     <NavigationHeader activeTab = "Početna" />
     <Carousel/>
     <AboutUs/>
+    <BandMembers/>
     <Footer/>
   </main>
 )
