c615c5e49bee0ee8745a652b46e99e0704aafa55
Kristina <kramlj00@fesb.hr>
2020-12-18 17:26:32 +0100

    "Adding About Us"

---
+++ src/components/AboutUs/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+import styles from './style.module.css'
+
+const AboutUs = () => (
+    <nav>
+        <h2 className={styles.aboutUsTitle}>O nama</h2>
+        <p className={styles.aboutUsText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Nunc tincidunt placerat dui quis semper. Mauris gravida sem massa, eu condimentum nunc pharetra nec. 
+        Ut vel neque eget erat congue sagittis. Vestibulum ullamcorper in mauris eget placerat.
+         Proin elementum tincidunt libero, sit amet dapibus orci fermentum sit amet. Curabitur volutpat sodales augue quis sagittis. 
+         Cras non risus sagittis, sagittis ipsum id, dictum elit. Sed facilisis nec nulla vel fringilla. 
+         Cras viverra pellentesque ipsum, quis euismod elit tristique sed. Nullam varius magna id auctor sagittis. 
+         Curabitur sodales at nunc mattis rhoncus. Nullam id diam justo.
+        </p>
+    </nav>
+)
+ 
+export default AboutUs
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import NavigationHeader from '../components/NavigationHeader'
 import Footer from '../components/Footer'
 import Carousel from '../components/Carousel'
+import AboutUs from '../components/AboutUs'
 
 const IndexPage = () => (
   <main>
     <NavigationHeader activeTab = "Početna" />
     <Carousel/>
+    <AboutUs/>
     <Footer/>
   </main>
 )
