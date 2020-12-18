30d82550189456f6d8f217edd089a5960e3c04e8
Kristina <kramlj00@fesb.hr>
2020-12-18 18:25:55 +0100

    "Adding ContactForm"

--- src/components/ContactTitle/index.js
+++ src/components/ContactTitle/index.js
@@ -5,7 +5,7 @@ import contactImg from '../../images/contactImg.jpg'
 
 const ContactTitle = () => (
     <nav>
-        <img src={contactImg} alt="contact_img" ></img>
+        <img src={contactImg} alt="contact_img" className={styles.contactImg}></img>
         <h2 className={styles.contactTitle}>Kontakt</h2>
     </nav>
 )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,13 +2,14 @@ import React from "react"
 import Footer from "../components/Footer"
 import NavigationHeader from '../components/NavigationHeader'
 import ContactTitle from '../components/ContactTitle'
-//import ContactForm from '../components/ContactForm'
+import ContactForm from '../components/ContactForm'
  
 const ContactPage = () => (
   <main>
     <NavigationHeader activeTab="Kontakt"/>
         <ContactTitle/>
-   
+        <ContactForm/>
+        <br></br>
     <Footer/>
   </main>
 )
