d6e02108fba00a43229581f085f74ec5d663f005
Petra Tadić <Petra>
2020-12-20 18:40:51 +0100

    editing blogpost.js and styling it

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,23 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styles from "./style.module.css"
+import NavigationHeader from "../components/NavigationHeader"
+import Footer from "../components/Footer"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
 
+
   return (
     <div>
+      <NavigationHeader/>
       <h1>{post.frontmatter.title}</h1>
-      <small>{post.frontmatter.date}</small>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </div>
+      <small>
+            {post.frontmatter.author}, {post.frontmatter.date}
+          </small>
+          <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.html }} />
+          <Footer/>
+          </div>
   )
 }
 
@@ -18,7 +26,9 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
+        date(fromNow: true)
         title
+        author  
       }
     }
   }
