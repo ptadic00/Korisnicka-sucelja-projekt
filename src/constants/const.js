579030be3303c1901170ce2640c6573d27c58da4
Petra Tadić <Petra>
2020-12-21 11:02:07 +0100

    linking listen.js to navbar

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,7 +1,7 @@
 export const navs = [
     {tab: 'Početna', to: '/'},
     {tab: 'Repertoar', to: '/'}, 
-    {tab: 'Poslušajte nas', to: '/'}, 
+    {tab: 'Poslušajte nas', to: '/listen'}, 
     {tab: 'Galerija', to: '/'}, 
     {tab: 'Blog', to: '/'}, 
     {tab: 'Kontakt', to: '/contact'}
