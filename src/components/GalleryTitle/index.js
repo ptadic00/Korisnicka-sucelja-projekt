e2efc7bf43bcbf2dcef188a95e8923fded4ffb25
Kristina <kramlj00@fesb.hr>
2021-01-04 14:33:01 +0100

    Adding GalleryTitle

---
+++ src/components/GalleryTitle/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styles from './style.module.css'
+
+const GalleryTitle = () => (
+    <nav >
+        <h1 className={styles.galleryTitle}>Galerija</h1>
+        <h2 className={styles.subtitle}>Ovdje možete pronaći naše slike s vjenčanja, domjenaka i evenata.</h2>
+    </nav>
+)
+ 
+export default GalleryTitle
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -2,12 +2,14 @@ import React from "react"
 import Footer from '../components/Footer'
 import NavigationHeader from '../components/NavigationHeader'
 import Photos from '../components/Gallery'
+import GalleryTitle from '../components/GalleryTitle'
 import 'react-image-lightbox/style.css';
 
 const GalleryPage = () => (
   <main>
     <NavigationHeader activeTab = "Galerija" />
     <style>{'body { background-color: black; }'}</style>
+    <GalleryTitle/>
     <Photos/>
     <Footer/>
   </main>
