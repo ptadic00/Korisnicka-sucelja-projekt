50f6da87c434a738adb004e7e5a47120cd236e9b
Kristina <kramlj00@fesb.hr>
2021-01-06 13:26:56 +0100

    Adding effects to listen us

--- src/components/listenUs/index.js
+++ src/components/listenUs/index.js
@@ -3,22 +3,31 @@ import ReactPlayer from "react-player"
 import Logo from "../Logo"
 import styles from "./style.module.css"
 
+import { Bounce } from "react-awesome-reveal";
+import { Slide  } from "react-awesome-reveal";
+import { Fade } from "react-awesome-reveal";
+
 const PoslusajteNas = () => (
   <div className={styles.pozadina}>
-    <div className={styles.title}>
-      <Logo />
-    </div>
+    <Bounce triggerOnce={true}>
+      <div className={styles.title}>
+        <Logo />
+      </div>
+    </Bounce>
+    
     <div className={styles.content}>
+    <Fade triggerOnce={true} delay={700}>
       <ReactPlayer url="https://www.youtube.com/watch?v=NPbbLM7TFCk" />
-      <br></br>
-      <br></br>
-      <ReactPlayer
-        className={styles.player}
-        url="https://www.youtube.com/watch?v=LJ4OtG_NrW4"
-      />
-      <br></br>
-      <br></br>
-      <ReactPlayer url="https://www.youtube.com/watch?v=fKxHgF3oNhU" />
+        <br></br>
+        <br></br>
+        <ReactPlayer
+          className={styles.player}
+          url="https://www.youtube.com/watch?v=LJ4OtG_NrW4"
+        />
+        <br></br>
+        <br></br>
+        <ReactPlayer url="https://www.youtube.com/watch?v=fKxHgF3oNhU" />
+      </Fade>
     </div>
   </div>
 )
