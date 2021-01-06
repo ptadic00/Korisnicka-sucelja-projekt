ebbc373036d10cca601e8a02eee3830b6b30e868
Kristina <kramlj00@fesb.hr>
2021-01-06 13:25:31 +0100

    Adding effects to contact

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -4,55 +4,59 @@ import styles from './style.module.css';
 import faceLogo from '../../images/faceLogo.png'
 import instagramLogo from '../../images/instagramLogo.png'
 
+import { Fade } from "react-awesome-reveal";
+
 const ContactForm = () => (
     <main className={styles.contactForm}>
-        <section className={styles.contactInfo}>
- 
-            <h2>KONTAKT</h2>
-            <p></p>
-            <h3>broj mobitela (Filip Matić)</h3>
-            <p></p>
-            <p>+385 091 5214 758</p>
- 
-            <h3>email</h3>
-            <p></p>
-            <p>bendzasvadbe@email.com</p>
- 
-            <p>IBAN: HR08497593759385739574</p>
+        <Fade triggerOnce={true} delay={300}>
+            <section className={styles.contactInfo}>
+    
+                <h2>KONTAKT</h2>
+                <p></p>
+                <h3>broj mobitela (Filip Matić)</h3>
+                <p></p>
+                <p>+385 091 5214 758</p>
+    
+                <h3>email</h3>
+                <p></p>
+                <p>bendzasvadbe@email.com</p>
+    
+                <p>IBAN: HR08497593759385739574</p>
 
-            <section className={styles.socialNetw} >
-                <img src={instagramLogo} alt="instagramLogo" className={styles.instagramLogo}></img>
-                <img src={faceLogo} alt="faceLogo" className={styles.faceLogo}></img>
+                <section className={styles.socialNetw} >
+                    <img src={instagramLogo} alt="instagramLogo" className={styles.instagramLogo}></img>
+                    <img src={faceLogo} alt="faceLogo" className={styles.faceLogo}></img>
+                </section>
+                
+                
             </section>
-            
-            
-        </section>
-        <section className={styles.form}>
-            <h2 className={styles.title}>KONTAKT FORMA</h2>
-            <form>
-                <div>
-                    <label htmlFor="name">Ime</label>
-                    <input name="name" type="text" />
-                </div>
-                <div>
-                    <label htmlFor="lastname">Prezime</label>
-                    <input name="lastname" type="text" />
-                </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input name="email" type="email" />
-                </div>
-                <div>
-                    <label htmlFor="phone">Broj mobitela</label>
-                    <input name="phone" type="tel" />
-                </div>
-                <div className={styles.message}>
-                    <label htmlFor="message">Poruka</label>
-                    <input name="message" type="text" />
-                </div>
-                <button>Pošalji poruku</button>
-            </form>
-        </section>
+            <section className={styles.form}>
+                <h2 className={styles.title}>KONTAKT FORMA</h2>
+                <form>
+                    <div>
+                        <label htmlFor="name">Ime</label>
+                        <input name="name" type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="lastname">Prezime</label>
+                        <input name="lastname" type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input name="email" type="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="phone">Broj mobitela</label>
+                        <input name="phone" type="tel" />
+                    </div>
+                    <div className={styles.message}>
+                        <label htmlFor="message">Poruka</label>
+                        <input name="message" type="text" />
+                    </div>
+                    <button>Pošalji poruku</button>
+                </form>
+            </section>
+        </Fade>
     </main>
 )
  
--- src/components/ContactTitle/index.js
+++ src/components/ContactTitle/index.js
@@ -3,11 +3,15 @@ import React from 'react'
 import styles from './style.module.css'
 import contactImg from '../../images/contactImg.jpg'
 
+import { Bounce } from "react-awesome-reveal";
+
 const ContactTitle = () => (
     <nav>
         <img src={contactImg} alt="contact_img" className={styles.contactImg}></img>
-        <h2 className={styles.contactTitle}>Kontakt</h2>
-        <h3 className={styles.contactSubtitle}>Sve informacije o nama možete dobiti putem maila, broja telefona i naših društvenih mreža.</h3>
+        <Bounce triggerOnce={true}>
+            <h2 className={styles.contactTitle}>Kontakt</h2>
+            <h3 className={styles.contactSubtitle}>Sve informacije o nama možete dobiti putem maila, broja telefona i naših društvenih mreža.</h3>
+        </Bounce>
     </nav>
 )
  
